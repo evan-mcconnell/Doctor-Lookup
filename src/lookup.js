9c2e29c777c9f62c946edfe1030af1721ccc59eb
Kimberly McConnell <kmcconnell393@gmail.com>
2019-03-22 11:51:10 -0700

    add searchterms submit which returns the queried doctor list to screen with info

--- src/lookup.js
+++ src/lookup.js
@@ -4,7 +4,7 @@ export class DoctorsByCondition {
   getDoctorList(name,cond) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${cond}&location=or-portland&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=1eed1e7d094218f3572acbb12421b2f4`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${cond}&location=or-portland&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
@@ -23,13 +23,15 @@ export class DoctorsByCondition {
     });
     return doctors;
   }
-  // newPatients(practices) {
-  //   practices.forEach((pract) => {
-  //     if (pract.accepts_new_patients === true) {
-  //       return `The office at ${pract.visit_address.street} in ${pract.visit_address.city} is accepting new patients. We'd be happy to schedule you!`
-  //     } else {
-  //       return `The office at ${pract.visit_address.street} in ${pract.visit_address.city} is not accepting new patients at this time.`
-  //     }
-  //   });
-  // }
+  newPatients(practices) {
+    let newP = [];
+    practices.forEach((pract) => {
+      if (pract.accepts_new_patients === true) {
+        newP.push(`The office at ${pract.visit_address.street} in ${pract.visit_address.city} is accepting new patients.<br>`);
+      } else {
+        newP.push(`The office at ${pract.visit_address.street} in ${pract.visit_address.city} is not accepting new patients at this time.<br>`);
+      }
+    });
+    return newP;
+  }
 }
--- src/main.js
+++ src/main.js
@@ -4,39 +4,32 @@ import $ from 'jquery';
 import './sass/styles.scss';
 import { DoctorsByCondition } from './lookup';
 
-function newPatients(practices) {
-  let newP = [];
-  practices.forEach((pract) => {
-    if (pract.accepts_new_patients === true) {
-      newP.push(`The office at ${pract.visit_address.street} in ${pract.visit_address.city} is accepting new patients.<br>`);
-    } else {
-      newP.push(`The office at ${pract.visit_address.street} in ${pract.visit_address.city} is not accepting new patients at this time.<br>`);
-    }
-  });
-  return newP;
-}
 
 $(document).ready(function() {
 
-  let name = "john";
-  let cond = "pain";
+  $("#doctor-search").submit(function(event) {
+    event.preventDefault();
+    const cond = $("#condition").val();
+    const name = $("#doctor").val();
+    console.log(name);
 
-  let docLookup = new DoctorsByCondition();
-  let promise1 = docLookup.getDoctorList(name, cond);
 
-  promise1.then((response) => {
-    let body = JSON.parse(response);
-    let docList = docLookup.getList(body.data);
-    console.log(newPatients(body.data[0].practices));
-    console.log(body.data[6].practices[0].visit_address.street);
-    console.log(body.data[6].practices[0].visit_address.street2);
-    console.log(body.data[6].practices[0].visit_address.city);
-    console.log(body.data[6].practices[0].visit_address.state);
-    console.log(body.data[6].practices[0].visit_address.zip);
+    let docLookup = new DoctorsByCondition();
+    let promise1 = docLookup.getDoctorList(name, cond);
 
-    docList.forEach((doc) => {
-      // const newPatients
-      $("#docs-in-area-list").append(`<li><img src="${doc.profile.image_url}" alt=""><h4 class="doc-name">${doc.profile.first_name} ${doc.profile.last_name}, ${doc.profile.title}</h4><address class="doc-address">${doc.practices[0].visit_address.street}${doc.practices[0].visit_address.street2}<br>${doc.practices[0].visit_address.city}, ${doc.practices[0].visit_address.state} ${doc.practices[0].visit_address.zip}</address><p class="doc-phone">${doc.practices[0].phones[0].type} ${doc.practices[0].phones[0].number}</p><a class="" href=""></a><p>${newPatients(doc.practices)}</p></li>`)
+    promise1.then((response) => {
+      let body = JSON.parse(response);
+      let docList = docLookup.getList(body.data);
+      console.log(body.data);
+      // console.log(body.data[6].practices[0].visit_address.street2);
+      // console.log(body.data[6].practices[0].visit_address.city);
+      // console.log(body.data[6].practices[0].visit_address.state);
+      // console.log(body.data[6].practices[0].visit_address.zip);
+
+      docList.forEach((doc) => {
+        const newPatientList = docLookup.newPatients(doc.practices);
+        $("#docs-in-area-list").append(`<li><img src="${doc.profile.image_url}" alt=""><h4 class="doc-name">${doc.profile.first_name} ${doc.profile.last_name}, ${doc.profile.title}</h4><address class="doc-address">${doc.practices[0].visit_address.street}${doc.practices[0].visit_address.street2}<br>${doc.practices[0].visit_address.city}, ${doc.practices[0].visit_address.state} ${doc.practices[0].visit_address.zip}</address><p class="doc-phone">${doc.practices[0].phones[0].type} ${doc.practices[0].phones[0].number}</p><a class="" href=""></a><p>${newPatientList.toString().replace(/,/g, '')}</p></li>`)
+      });
     });
   });
 });
