a71903d1bc79a5afe069f00f4bf65eb9f1f882fc
Kimberly McConnell <kmcconnell393@gmail.com>
2019-03-22 09:28:06 -0700

    add a jasmine test for doctor api GET using (done) to handle async

--- spec/test-spec.js
+++ spec/test-spec.js
@@ -1,3 +1,27 @@
-it ('returns true', () => {
-  expect(true).toEqual(true);
-})
+import { DoctorsByCondition } from './../src/lookup.js';
+
+const nameSearch = "john";
+const cond = "pain";
+let docLookup = new DoctorsByCondition();
+let promise1 = docLookup.getDoctorList(nameSearch, cond);
+
+let name;
+
+function testPromise1(done) {
+promise1.then(function(response) {
+    let body = JSON.parse(response);
+    name = body.data[0].profile.first_name;
+    done();
+  });
+}
+
+
+describe('DoctorsByCondition', function() {
+  beforeEach(function(done) {
+    testPromise1(done);
+  });
+
+  it('should return the first name of the first doctor in the search', function() {
+    expect(name).toEqual("John");
+  });
+});
